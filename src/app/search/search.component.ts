efe676dcf51975a196ee1b468dd50b5a5f1552d1
bilalkh1 <bilal.khatab@outlook.fr>
2021-12-21 19:22:27 +0100

    feat: Working on search page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+  isOpened: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCloseSideNav(e) {
+    if (e.target.classList.contains("sidenav-container")) {
+      this.isOpened = false;
+      document.body.className = "";
+    }
+  }
+
+  onSideNavToggle() {
+    this.isOpened = !this.isOpened;
+    document.body.className = "overflow-hidden-mobile";
   }
 
 }
