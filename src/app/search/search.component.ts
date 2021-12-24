6983cb31ca12f8dd8a3d66dbcf733aa3cd92883a
Mehdi Chiboub <mehdichiboub.98@gmail.com>
2021-12-24 17:18:18 +0100

    feat: redesign of search page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,5 @@
 import { SearchService } from './../shared/services/search.service';
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -9,53 +7,50 @@ import { HttpEventType } from '@angular/common/http';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  isOpened: boolean = false;
+  isOpened = false;
   image: string;
+  images = [];
   file: File;
+
+
   constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
   }
 
-  onCloseSideNav(e) {
-    if (e.target.classList.contains("sidenav-container")) {
-      this.isOpened = false;
-      document.body.className = "";
-    }
-  }
-
-  onSideNavToggle() {
-    this.isOpened = !this.isOpened;
-    document.body.className = "overflow-hidden-mobile";
-  }
-
-  onSubmit() {
+  // Send the uploaded image to the server and catch resulting images
+  onSubmit(): void {
     const formData = new FormData();
     formData.append('image', this.file);
-    console.log(formData);
     this.searchService
-    .uploadFile(formData)
-    .subscribe((res) => {
-      console.log(res);
-    });
+      .uploadFile(formData)
+      .then((res: any) => {
+        this.images = res.split('/');
+        console.log(this.images);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+    this.isOpened = ! this.isOpened;
   }
 
    // FileUpload
-   readUrl(event: any) {
-    if (event.target.files.length === 0)
+   readUrl(event: any): void {
+    if (event.target.files.length === 0) {
       return;
-    //Image upload validation
-    var mimeType = event.target.files[0].type;
+    }
+    // Image upload validation
+    const mimeType = event.target.files[0].type;
     if (mimeType.match(/image\/*/) == null) {
       return;
     }
     // Image upload
     const file: File = event.target.files[0];
-    console.log(event.target.value.split('.').pop());
-
-    var reader = new FileReader();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.image = reader.result as string;
+    };
     reader.readAsDataURL(file);
-    console.log(file);
     this.file = file;
   }
 
--- src/app/shared/services/search.service.ts
+++ src/app/shared/services/search.service.ts
@@ -8,8 +8,10 @@ export class SearchService {
 
   constructor(private http: HttpClient) { }
 
-  uploadFile(img: FormData) {
-    return this.http.post(`http://127.0.0.1:5000/uploader`, img);
+  uploadFile(img: FormData): any {
+    return this.http
+      .post(`http://127.0.0.1:5000/uploader`, img, {responseType: 'text'})
+      .toPromise();
   }
 
 }
